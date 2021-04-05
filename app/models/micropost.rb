5fd7bfd72676d8565468f48ad67ff1fa72abd925
Omer <omer@sdilabsinc.com>
2021-04-05 07:02:51 -0400

    some style

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,6 +1,6 @@
 class Micropost < ApplicationRecord
   belongs_to :user
-  has_many :comments
+  has_many :comments, :dependent => :destroy
   validates :content, length: { maximum: 140 }, presence: true
   validates :user, presence: true
 end
