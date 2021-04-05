8b5a28d7c75305aec7977a47bcb6738e417323bc
Omer <omer@sdilabsinc.com>
2021-04-05 07:06:52 -0400

    associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_many :microposts
-  has_many :comments
+  has_many :microposts, :dependent => :destroy
+  has_many :comments, :dependent => :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :recoverable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
